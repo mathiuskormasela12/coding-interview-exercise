ca853dab8a3fbb8ba52df3d7348a3e32cf90e781
Mathius <mathiuskormasela12@gmail.com>
2023-07-11 16:31:52 +0700

    init project

---
+++ test/main.test.ts
@@ -0,0 +1,9 @@
+// ========== Main Test
+// import all modules)
+import { fizzBuzz } from '../src/main'
+
+describe('FizzBuzz', () => {
+  it('should return "FizzBuzz"', () => {
+    expect(fizzBuzz()).toBe('FizzBuzz')
+  })
+})
