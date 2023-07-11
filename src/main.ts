d97a7f622c627d588bb5e8b519454c2f0fabaf53
Mathius <mathiuskormasela12@gmail.com>
2023-07-11 16:57:08 +0700

    fizzbuzz

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
 // =========== Main
 
-export const fizzBuzz = (): string => 'FizzBuzz'
+// FizzBuzz
+export type FizzBuzzReturnValue = 'Fizz' | 'Buzz' | 'FizzBuzz' | number
+type FizzBuzz = (total: number) => FizzBuzzReturnValue[]
+
+export const fizzBuzz: FizzBuzz = (total) => {
+  const result: FizzBuzzReturnValue[] = []
+
+  for (let i = 1; i <= total; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      result.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      result.push('Fizz')
+    } else if (i % 5 === 0) {
+      result.push('Buzz')
+    } else {
+      result.push(i)
+    }
+  }
+
+  return result
+}
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,26 @@
 // ========== Main Test
 // import all modules)
-import { fizzBuzz } from '../src/main'
+import { type FizzBuzzReturnValue, fizzBuzz } from '../src/main'
 
 describe('FizzBuzz', () => {
   it('should return "FizzBuzz"', () => {
-    expect(fizzBuzz()).toBe('FizzBuzz')
+    const expectedResult: FizzBuzzReturnValue[] = [
+      1,
+      2,
+      'Fizz',
+      4,
+      'Buzz',
+      'Fizz',
+      7,
+      8,
+      'Fizz',
+      'Buzz',
+      11,
+      'Fizz',
+      13,
+      14,
+      'FizzBuzz'
+    ]
+    expect(fizzBuzz(15)).toEqual(expectedResult)
   })
 })
