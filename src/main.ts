7690ec9e0549fb20fe3d1861e764cd93ad04a456
Mathius <mathiuskormasela12@gmail.com>
2023-07-11 18:01:59 +0700

    FizzBuzz Recursive

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 // FizzBuzz
 export type FizzBuzzReturnValue = 'Fizz' | 'Buzz' | 'FizzBuzz' | number
 type FizzBuzz = (total: number) => FizzBuzzReturnValue[]
+type FizzBuzzRecursive = (init: number, total: number, result: FizzBuzzReturnValue[]) => FizzBuzzReturnValue[]
 
 export const fizzBuzz: FizzBuzz = (total) => {
   const result: FizzBuzzReturnValue[] = []
@@ -20,4 +21,22 @@ export const fizzBuzz: FizzBuzz = (total) => {
   }
 
   return result
+}
+
+export const fizzBuzzRecursive: FizzBuzzRecursive = (init, total, result): FizzBuzzReturnValue[] => {
+  if ((init % 3 === 0) && (init % 5 === 0)) {
+    result.push('FizzBuzz')
+  } else if (init % 3 === 0) {
+    result.push('Fizz')
+  } else if (init % 5 === 0) {
+    result.push('Buzz')
+  } else {
+    result.push(init)
+  }
+
+  if (init === total) {
+    return result
+  } else {
+    return fizzBuzzRecursive(init + 1, total, result)
+  }
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,26 +1,19 @@
 // ========== Main Test
 // import all modules)
-import { type FizzBuzzReturnValue, fizzBuzz } from '../src/main'
+import { type FizzBuzzReturnValue, fizzBuzz, fizzBuzzRecursive } from '../src/main'
 
 describe('FizzBuzz', () => {
-  it('should return "FizzBuzz"', () => {
-    const expectedResult: FizzBuzzReturnValue[] = [
-      1,
-      2,
-      'Fizz',
-      4,
-      'Buzz',
-      'Fizz',
-      7,
-      8,
-      'Fizz',
-      'Buzz',
-      11,
-      'Fizz',
-      13,
-      14,
-      'FizzBuzz'
-    ]
+  const expectedResult: FizzBuzzReturnValue[] = [
+    1, 2, 'Fizz', 4, 'Buzz',
+    'Fizz', 7, 8, 'Fizz', 'Buzz',
+    11, 'Fizz', 13, 14, 'FizzBuzz'
+  ]
+
+  it('should return "FizzBuzz" (Looping)', () => {
     expect(fizzBuzz(15)).toEqual(expectedResult)
+  })
+
+  it('should return "FizzBuzz" (Recursive)', () => {
+    expect(fizzBuzzRecursive(1, 15, [])).toEqual(expectedResult)
   })
 })
